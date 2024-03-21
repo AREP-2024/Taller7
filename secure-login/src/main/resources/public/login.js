95dce4f0ca8c3fa0006be47b9c5d6d13f1b51cb9
LuisaGiron <luisa.bermudez@mail.escuelaing.edu.co>
2024-03-20 20:37:01 -0500

    proyecto secure-login terminado

--- secure-login/src/main/resources/public/login.js
+++ secure-login/src/main/resources/public/login.js
@@ -2,11 +2,36 @@ function login() {
     let inputusername = document.getElementById("username");
     let inputpassword = document.getElementById("password");
     let endPoint = "/login";
-    let llamado = llamarAPI(endPoint,cuerpo);
+    let llamado = llamarAPI(endPoint,{
+        username: inputusername.value,
+        password: inputpassword.value
 
+    });
+    llamado.then((resultado)=>{
+        let divRespuesta = document.getElementById("respuesta");
+        const aux = Boolean(resultado);
 
+        divRespuesta.innerHTML= aux?"Bienvenido!! 👌":"Usuario o contrasena incorrectos 🤔";
+
+    });
 }
 
 async function llamarAPI(endPoint,cuerpo){
+    return await fetch(window.location.origin+endPoint,{
+        headers:{
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: cuerpo
+    })
+    .then((response)=>{
+        if(!response.ok){
+            throw new Error();
+        }
+        return response.text();
+    })
+
+    .catch((error)=> alert(error));
+
 
 }
